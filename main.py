65f481851f63f3964c42f734a9718ef0e452548f
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-07-07 21:06:18 -0700

    Print sout map bans now

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ def main():
     all_map_bans = {"Villa": 0, "Oregon": 0, "Kafe": 0, "Coastline": 0, "Clubhouse": 0, "Themepark": 0, '': 0, "Consulate": 0 } # All the maps banned
 
     all_map_picks = {"Villa": 0, "Oregon": 0, "Kafe": 0, "Coastline": 0, "Clubhouse": 0, "Themepark": 0, '': 0, "Consulate": 0 } # All the maps picked
+
+
+
     with open('.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(f'\nWelcome to Snake Finder v0.1.5\n\nAll column names are {", ".join(row)}\n')
+                print(f'\nWelcome to Snake Finder v0.1.7\n\nAll column names are {", ".join(row)}\n')
                 line_count += 1
             else:
                 all_map_bans[(row[8])] += 1
@@ -17,13 +20,20 @@ def main():
                 all_map_bans[(row[10])] += 1
                 all_map_bans[(row[11])] += 1
                 all_map_picks[(row[12])] += 1
+                
                 line_count += 1
         print(f'Done reading {line_count} lines')
 
     print("\n--------------------------------\n\n\tMAP PICKS:\n")
     for key, value in all_map_picks.items():
         if key != '':
-            print(f'{key} picked {value} times') 
+            print(f'{key} picked {value} times')
+
+    print("\n\tMAP BANS:\n\t/(Both?)\\\n")
+
+    for key, value in all_map_bans.items():
+        if key != '':
+            print(f'{key} banned {value} times')
 
 
 
